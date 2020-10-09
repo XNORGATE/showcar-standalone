d6f77a098999dc72e8651d16a46373a92d52d528
wulinworks <52878239+wulinworks@users.noreply.github.com>
2020-10-09 22:17:52 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,18 +9,16 @@ Citizen.CreateThread(function()
 
     while 1 do
         inVision = false
-        local sleep = true
         local pCoords = GetEntityCoords(PlayerPedId())
         for i, c in ipairs(Cars) do
-            if #(pCoords - c.pos) < ShowRange then
+            if #(pCoords - c.pos) < ShowRange and spawned[i] == nil then
                 SpawnLocalCar(i, c)
-                sleep = false
             elseif spawned[i] ~= nil then
                 DeleteEntity(spawned[i])
                 spawned[i] = nil
             end
         end
-        _ = sleep and Wait(500) or Wait(0)
+        Wait(500)
     end
 end)
 
@@ -36,25 +34,27 @@ Citizen.CreateThread(function()
 end)
 
 function SpawnLocalCar(i, c)
-    local hash = GetHashKey(c.model)
+    Citizen.CreateThread(function()
+        local hash = GetHashKey(c.model)
 
-    RequestModel(hash)
-    local attempt = 0
-    while not HasModelLoaded(hash) do
-        attempt = attempt + 1
-        _ = attempt > 2000 and return or Wait(0)
-    end
+        RequestModel(hash)
+        local attempt = 0
+        while not HasModelLoaded(hash) do
+            attempt = attempt + 1
+            _ = attempt > 2000 and return or Wait(0)
+        end
 
-    local veh = CreateVehicle(hash, c.pos, c.heading, false, false)
-	SetModelAsNoLongerNeeded(hash)
-    SetVehicleEngineOn(veh, true, true, true)
-    SetVehicleBrakeLights(veh, true)
-    SetVehicleLights(veh, 2)
-    SetVehicleLightsMode(veh, 2)
-    SetVehicleInteriorlight(veh, true)
-    SetVehicleOnGroundProperly(veh)
-    Wait(100)
-    FreezeEntityPosition(veh, true)
+        local veh = CreateVehicle(hash, c.pos, c.heading, false, false)
+        SetModelAsNoLongerNeeded(hash)
+        SetVehicleEngineOn(veh, true, true, true)
+        SetVehicleBrakeLights(veh, true)
+        SetVehicleLights(veh, 2)
+        SetVehicleLightsMode(veh, 2)
+        SetVehicleInteriorlight(veh, true)
+        SetVehicleOnGroundProperly(veh)
+        Wait(100)
+        FreezeEntityPosition(veh, true)
 
-    spawned[i] = veh
+        spawned[i] = veh
+    end)
 end
