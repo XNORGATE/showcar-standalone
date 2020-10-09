893b4a7ef0ca555efcf5d0cac03314d69529cc7a
wulinworks <52878239+wulinworks@users.noreply.github.com>
2020-10-09 22:30:49 +0800

    fix bugs

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local spawned = {}
+spawned = {}
 
 Citizen.CreateThread(function()
 	while not ESX do
@@ -8,14 +8,15 @@ Citizen.CreateThread(function()
     while not ESX.IsPlayerLoaded() do Wait(500) end
 
     while 1 do
-        inVision = false
         local pCoords = GetEntityCoords(PlayerPedId())
-        for i, c in ipairs(Cars) do
-            if #(pCoords - c.pos) < ShowRange and spawned[i] == nil then
-                SpawnLocalCar(i, c)
-            elseif spawned[i] ~= nil then
-                DeleteEntity(spawned[i])
-                spawned[i] = nil
+        for i=1, #Cars do
+            if #(pCoords - Cars[i].pos) < ShowRange then
+                if Cars[i].spawned == nil then
+                    SpawnLocalCar(i)
+                end
+            elseif Cars[i].spawned ~= nil then
+                DeleteEntity(Cars[i].spawned)
+                Cars[i].spawned = nil
             end
         end
         Wait(500)
@@ -24,27 +25,28 @@ end)
 
 Citizen.CreateThread(function() 
     while 1 do
-        for i, c in pairs(spawned) do
-            if Cars[i].spin then
-                SetEntityHeading(c, GetEntityHeading(c) - 0.3)
+        for i=1, #Cars do
+            if Cars[i].spawned ~= nil and Cars[i].spin then
+                SetEntityHeading(Cars[i].spawned, GetEntityHeading(Cars[i].spawned) - 0.3)
             end
         end
         Wait(5)
     end
 end)
 
-function SpawnLocalCar(i, c)
+function SpawnLocalCar(i)
     Citizen.CreateThread(function()
-        local hash = GetHashKey(c.model)
+        local hash = GetHashKey(Cars[i].model)
 
         RequestModel(hash)
         local attempt = 0
         while not HasModelLoaded(hash) do
             attempt = attempt + 1
-            _ = attempt > 2000 and return or Wait(0)
+            if attempt > 2000 then return end
+            Wait(0)
         end
 
-        local veh = CreateVehicle(hash, c.pos, c.heading, false, false)
+        local veh = CreateVehicle(hash, Cars[i].pos, Cars[i].heading, false, false)
         SetModelAsNoLongerNeeded(hash)
         SetVehicleEngineOn(veh, true, true, true)
         SetVehicleBrakeLights(veh, true)
@@ -52,9 +54,18 @@ function SpawnLocalCar(i, c)
         SetVehicleLightsMode(veh, 2)
         SetVehicleInteriorlight(veh, true)
         SetVehicleOnGroundProperly(veh)
-        Wait(100)
         FreezeEntityPosition(veh, true)
 
-        spawned[i] = veh
+        Cars[i].spawned = veh
     end)
 end
+
+AddEventHandler('onResourceStop', function(res)
+    if res == GetCurrentResourceName() then
+        for i=1, #Cars do
+            if Cars[i].spawned ~= nil then
+                DeleteEntity(Cars[i].spawned)
+            end
+        end
+    end
+end)
