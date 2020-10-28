784adf09476f3b212e727af34abd5e67ba0dc1fd
中汁龍 <72063585+XNORGATE@users.noreply.github.com>
2020-10-28 16:04:18 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,18 +1,25 @@
 ShowRange = 100
+
+DoorLock = false
+
+carInvincible = true 
+
 Cars = {
     {
         pos = vec(223.37, -841.97, 29.88),
         heading = 72.25,
         model = 'blista',
         spin = true,
-        text = "XA21"
+        text = "XA21",
+        plate = "Xa21"
     },
     {
         pos = vec(210.37, -841.97, 29.88),
         heading = 72.25,
         model = 'blista',
         spin = false,
-        text = "This is a vehicle"
+        text = "This is a vehicle",
+        plate = "blista"
     }
 }
 
