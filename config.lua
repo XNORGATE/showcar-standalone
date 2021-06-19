98c386b92c50d18f81801b1354d0826f586ff85e
Tsai <72063585+XNORGATE@users.noreply.github.com>
2021-06-19 22:39:25 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-ShowRange = 100
+ShowRange = 200
 
 DoorLock = false
 
@@ -6,25 +6,283 @@ carInvincible = true
 
 Cars = {
     {
-        pos = vec(223.37, -841.97, 29.88),
-        heading = 72.25,
-        model = 'blista',
-        spin = true,
-        text = "XA21",
-        plate = "Xa21"
+        pos = vec(-1181.18, -1724.65, 4.5), --inside 1
+        heading = 192.56,
+        model = 'sc1',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
     },
     {
-        pos = vec(210.37, -841.97, 29.88),
-        heading = 72.25,
-        model = 'blista',
+        pos = vec( -1175.50, -1720.53, 4.5), --inside 2
+        heading = 191.31,
+        model = 'italigtb2',
         spin = false,
-        text = "This is a vehicle",
-        plate = "blista"
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1169.66, -1716.91, 4.5), --inside 3
+        heading = 190.70,
+        model = 'infernus',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1163.27, -1712.13, 4.5), --inside 4
+        heading = 189.77,
+        model = 'entity2',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1156.91, -1707.65, 4.5), --inside 5
+        heading = 189.63,
+        model = 'bullet',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    --ramps--
+    {
+        pos = vec(-1115.05, -1719.84, 4.43), --trucks 1
+        heading = 260.94,
+        model = 'burrito3',
+        spin = false,
+        text = "burrito",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1118.60, -1722.75, 4.73), --trucks 2 
+        heading = 261.02,
+        model = 'surfer',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1124.04, -1726.83, 4.73), --trucks 3
+        heading = 169.44,
+        model = 'mesa3',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1128.32, -1728.55, 4.36), --trucks 4 
+        heading = 168.44,
+        model = 'bison',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+     --bikes--
+     {
+        pos = vec(-1139.38, -1714.60, 3.91), --bikes 1
+        heading = 246.28,
+        model = 'akuma',
+        spin = false,
+        text = "burrito",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1137.86, -1713.32, 3.91), --bikes 2 
+        heading = 239.74,
+        model = 'bati',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1135.97, -1712.13, 3.91), --bikes 3
+        heading = 246.37,
+        model = 'daemon2',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1133.24, -1709.65, 3.91), --bikes 4 
+        heading = 252.95,
+        model = 'faggio',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1130.87, -1708.14, 3.91), --bikes 5 
+        heading = 256.87,
+        model = 'lectro',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1128.89, -1706.60, 3.91), --bikes 6
+        heading = 248.03,
+        model = 'nightblade',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec( -1127.18, -1704.94, 3.91), --bikes 7 
+        heading = 254.28,
+        model = 'vindicator',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    --Classic
+    {
+        pos = vec(-1132.28, -1718.89, 3.91), --class 1
+        heading = 212.21,
+        model = 'deviant',
+        spin = false,
+        text = "burrito",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1129.46, -1717.12, 3.91), --class 2
+        heading = 213.04,
+        model = 'dominator',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1126.41, -1715.30, 3.91), --class 3
+        heading = 215.23,
+        model = 'gauntlet',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1123.80, -1713.19, 3.91), --class 4
+        heading = 218.27,
+        model = 'dukes',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1120.80, -1711.17, 3.91), --class 5
+        heading = 214.87,
+        model = 'ellie',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    --suppers
+    {
+        pos = vec(-1146.89, -1717.78, 3.91), --angled super 1
+        heading = 162.31,
+        model = 'prototipo',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1150.51, -1715.4, 3.91), --angled super 2
+        heading = 155.41,
+        model = 'zentorno',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1164.55, -1737.38, 3.91), --super 1 front
+        heading = 217.23,
+        model = 'adder',
+        spin = false,
+        text = "burrito",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1160.31, -1734.83, 3.91), --super 2 front
+        heading = 215.55,
+        model = 'f620',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1156.61, -1732.17, 3.91), --super 3 front
+        heading = 215.58,
+        model = 'exemplar',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1168.04, -1732.64, 3.91), --super 4 back
+        heading = 216.51,
+        model = 'windsor2',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1163.68, -1729.82, 3.91), --super 5 back
+        heading = 217.09,
+        model = 'huntley',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1160.53, -1726.68, 3.91), --super 6 back
+        heading = 218.01,
+        model = 'patriot',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    --shitboxes
+    {
+        pos = vec(-1144.05, -1738.40, 3.91), --shitbox 1
+        heading = 36.43,
+        model = 'premier',
+        spin = false,
+        text = "burrito",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1147.89, -1740.68, 3.91), --shitbox 2 
+        heading = 37.42,
+        model = 'asea',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1151.06, -1743.17, 3.91), --shitbox 3
+        heading = 35.35,
+        model = 'tailgater',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1154.68, -1745.22, 3.91), --shitbox 4 
+        heading = 34.59,
+        model = 'habanero',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
+    },
+    {
+        pos = vec(-1158.14, -1747.64, 3.91), --shitbox 5 
+        heading = 33.87,
+        model = 'minivan',
+        spin = false,
+        text = "test",
+        plate = "DEALER"
     }
 }
-
-
-
 
 -----------------------------------------------------------------------------------幹你娘 不要拿去賣 幹你娘 不要拿去賣----------------------------------------------------------------------------------------------------------------------------------------
 --vec(223.37, -841.97, 29.88),
